246ede6ba3d68f4636d4eed3d3d623c6f249c292
François Terrier <fterrier@gmail.com>
2011-10-24 18:00:21 +0200

    added filter for MS ie6 and added static content pages

---
+++ grails-app/conf/IE6Filters.groovy
@@ -0,0 +1,24 @@
+import org.springframework.web.context.request.RequestContextHolder;
+
+
+class IE6Filters {
+
+	def filters = {
+		allURIs(uri:'/upgrade', invert:true) {
+			before = {
+				if (actionName != null && controllerName != null) {
+					
+					def userAgent = RequestContextHolder.currentRequestAttributes().currentRequest.getHeader('user-agent')
+					log.error(userAgent)
+					
+					if (userAgent.contains('MSIE 6.0')) {
+						redirect(uri:'/upgrade')
+						return false
+					}
+					
+				}
+			}
+		}
+	}
+	
+}
---
+++ grails-app/views/helpdesk.gsp
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="layout" content="main" />
+        <title><g:message code="about.page.label" default="About" /></title>
+    </head>
+    <body>
+    	<div class="subnav">
+    	
+    	</div>
+		<div class="main">
+			Having issues? Call the helpdesk (free call): 114
+		</div>
+    </body>
+</html>
