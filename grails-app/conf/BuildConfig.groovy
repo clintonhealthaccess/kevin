6e5be42f9889b216ba3aef0c3d0460a6e688024b
François Terrier <fterrier@gmail.com>
2011-11-20 11:27:17 +0200

    build config cleaned up dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
 //		mavenRepo "http://snapshots.repository.codehaus.org"
 //		mavenRepo "http://repository.codehaus.org"
 //		mavenRepo "http://www.intalio.org/public/maven2/"
-//		mavenRepo "http://repo.opennms.org/maven2/"
+		mavenRepo "http://repo.opennms.org/maven2/"
 //		mavenRepo "https://repository.jboss.org/nexus/content/groups/public-jboss/"
 				
 		/**
