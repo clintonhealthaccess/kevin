60cefcd35996068a23f92e5a5dcd047c958d2117
François Terrier <fterrier@gmail.com>
2012-02-10 17:43:10 +0200

    moved templates

---
+++ web-app/js/progressbar_init.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(".js_progress-bar").each(function(){
+		var values = $(this).html().split('/');
+		
+		if (values.length == 2) {
+			var value;
+			if(values[1] == 0) value = 0;
+			else value = (values[0]/values[1])*100;
+			$(this).progressBar(value, {
+				steps: 0,
+				boxImage: progress['boxImage'],
+				barImage: {
+					0:  progress['barImage_0'],
+					30:  progress['barImage_30'],
+					70:  progress['barImage_70'],
+				}
+			});
+		}
+	});
+});
