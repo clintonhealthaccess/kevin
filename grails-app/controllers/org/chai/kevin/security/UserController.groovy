325abf92f2e19591d22fe846edafae404d1d62dc
Jean Kahigiso Muchika <jkahigiso@gmail.com>
2012-02-21 17:05:00 +0200

    Latest change on UserAdminPassord

--- grails-app/controllers/org/chai/kevin/security/UserController.groovy
+++ grails-app/controllers/org/chai/kevin/security/UserController.groovy
@@ -1,5 +1,6 @@
 package org.chai.kevin.security
 
+import org.apache.shiro.crypto.hash.Sha256Hash
 import org.chai.kevin.AbstractEntityController
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
@@ -22,14 +23,22 @@ class UserController extends AbstractEntityController {
 	}
 
 	def getModel(def entity) {
-		[user:entity, roles: Role.list()]
+		[user:entity, roles: Role.list(), cmd: params['cmd']]
 	}
 
+	def validateEntity(def entity) {
+		return entity.validate() && params['cmd'].validate()
+	}
+	
 	def bindParams(def entity) {
-		entity.properties = params
+		if (log.isDebugEnabled()) log.debug('binding params: '+params)
+		bindData(entity,params,[exclude:['uuid','passwordHash']])
 		
-		if (params.passwordHash!=null && params.confirmPass.equals(params.passwordHash))
-			entity.passwordHash = params.passwordHash
+		if(entity.id==null)
+			entity.uuid = UUID.randomUUID().toString();
+			
+		if(params['cmd']?.password != null && !params['cmd']?.password.equals(''))
+			entity.passwordHash = new Sha256Hash(params['cmd'].password).toHex();
 	}
 	
 	def list = {
@@ -44,5 +53,28 @@ class UserController extends AbstractEntityController {
 			code: getLabel()
 		])
 	}
+	
+	def save = { PasswordCommand cmd ->
+		if (log.isDebugEnabled()) log.debug("create.userPassword, params:"+params+"command"+cmd)
+		params['cmd'] = cmd;
+		super.save()
+
+	}
+	
 		
+}
+class PasswordCommand {
+	String password
+	String repeat
+
+	static constraints = {
+		password(blank: true, minSize: 4)
+		repeat(validator: {val, obj ->
+			val == obj.password
+		})
+	}
+	
+	String toString() {
+		return "PasswordCommand[password="+password+", repeat="+repeat+"]"
+	}
 }
--- grails-app/views/entity/user/_createUser.gsp
+++ grails-app/views/entity/user/_createUser.gsp
@@ -6,15 +6,17 @@
 	</div>
 	<div class="data-field-column">
 		<g:form url="[controller:'user', action:'save', params: [targetURI: targetURI]]" useToken="true">
-		
+			<g:if test="${user.uuid != null}">
+				<input type="hidden" name="uuid" value="${user.uuid}"/>
+			</g:if>
 			<g:input name="username" label="Username" bean="${user}" field="username"/>
 			<g:input name="firstname" label="First name" bean="${user}" field="firstname"/>
 			<g:input name="lastname" label="Last name" bean="${user}" field="lastname"/>
 			<g:input name="location" label="Location" bean="${user}" field="location"/>
 			<g:input name="email" label="Email" bean="${user}" field="email"/>
 			<g:input name="permissionString" label="Permission" bean="${user}" field="permissionString"/>
-			<g:inputPass name="passwordHash" label="Password" bean="${user}" field="passwordHash"/>
-			<g:inputPass name="passwordHash" label="Repeat password"  field="passwordHash"/>
+			<g:inputPass name="password" label="Password"  bean="${cmd}" field="password"/>
+			<g:inputPass name="repeat" label="Repeat password" bean="${cmd}"  field="repeat"/>
 						
 			<div class="row">
 				<label><g:message code="user.confirmed.label" default="Confirmed"/></label>
@@ -30,7 +32,7 @@
 				from="${roles}" value="${user.roles*.id}" optionValue="name" optionKey="id" multiple="true"/>
 			
 			<g:if test="${user.id != null}">
-				<input type="hidden" name="id" value="${user.id}"></input>
+				<input type="hidden" name="id" value="${user.id}"/>
 			</g:if>
 			
 			<div class="row">
