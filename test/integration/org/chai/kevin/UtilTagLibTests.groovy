7cfb953eb15101e7002b9092a246fe07844d21ed
François Terrier <fterrier@gmail.com>
2012-02-22 15:06:32 +0200

    fixed issue where null was appended at the end of the url when there is no query

--- test/integration/org/chai/kevin/UtilTagLibTests.groovy
+++ test/integration/org/chai/kevin/UtilTagLibTests.groovy
@@ -26,4 +26,21 @@ class UtilTagLibTests extends GroovyPagesTestCase {
 		assertEquals "/kevin/test/test?targetURI=%2Ftest%2Ftest%3Ftest%3D123&test=123", url
 	}
 	
+	def testLinkWithTargetURIWhenNullQuery() {
+		def controller = new UserController()
+		
+		controller.request.queryString = null
+		// we assume that forwardURI is always the full URI after the hostname
+		controller.request.forwardURI = '/kevin/test/test'
+		// contextPath is always the path to the app
+		controller.request.contextPath = '/kevin'
+		controller.request.params = ['param': '123']
+		
+		def url = applyTemplate(
+			'<g:createLinkWithTargetURI controller="test" action="test" params="[test: 123]"/>'
+		)
+
+		assertEquals "/kevin/test/test?targetURI=%2Ftest%2Ftest&test=123", url
+	}
+	
 }
