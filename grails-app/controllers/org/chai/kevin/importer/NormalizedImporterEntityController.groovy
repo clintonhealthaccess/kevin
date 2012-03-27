a41552d467cadf572c4fbbb999cbd0a53e33dd08
Jean Kahigiso Muchika <jkahigiso@gmail.com>
2012-03-27 21:52:30 +0200

    raw data import plus test and views

---
+++ grails-app/controllers/org/chai/kevin/importer/NormalizedImporterEntityController.groovy
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2011, Clinton Health Access Initiative.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.chai.kevin.importer
+
+
+import java.io.InputStreamReader
+
+import org.chai.kevin.AbstractController
+import org.chai.kevin.data.RawDataElement;
+import org.chai.kevin.data.Type;
+import org.chai.kevin.value.RawDataElementValue;
+import org.hisp.dhis.period.Period
+import org.springframework.web.multipart.MultipartHttpServletRequest
+import org.springframework.web.multipart.commons.CommonsMultipartFile
+
+/**
+ * @author Jean Kahigiso M.
+ *
+ */
+class NormalizedImporterEntityController extends AbstractController {
+	ImporterService importerService;
+	final String IMPORT_FORM = "normalizedImport";
+	final String IMPORT_OUTPUT = "importOutput";
+	
+	def importer = {
+		this.getModel(null,null,IMPORT_FORM);
+	}
+	
+	def uploader = { NormalizedImporterEntityCommand cmd ->
+		ImporterErrorManager errorManager = new ImporterErrorManager();
+		if (!cmd.hasErrors()) {
+			if(log.isDebugEnabled()) log.debug("uploader(file="+cmd.file.getInputStream()+",period="+cmd.period+",dataElement="+cmd.dataElement+")")
+			InputStreamReader csvInputStreamReader = new InputStreamReader(cmd.file.getInputStream());
+			importerService.importNormalizedData(cmd.dataElement,csvInputStreamReader, cmd.period,errorManager);
+			this.getModel(cmd,errorManager,IMPORT_OUTPUT);
+		}else{
+			this.getModel(cmd,errorManager,IMPORT_FORM);
+		}	
+	}
+
+	def getModel(def cmd,ImporterErrorManager errorManager,String view) {
+		if(log.isDebugEnabled()) log.debug("getModel(cmd="+cmd+",errorManager="+errorManager+",view="+view+")")
+		
+		List<Period> periods = Period.list()
+		List<RawDataElement> dataElements =[]
+		if (cmd?.dataElement != null) dataElements << cmd.dataElement
+		render (view: '/import/'+view, model:[
+					periods: periods,
+					dataElements: dataElements,
+					importerEntity: cmd,
+					errorManager: errorManager
+				])
+	}
+}
+
+class NormalizedImporterEntityCommand {
+
+	Period period;
+	CommonsMultipartFile file;
+	RawDataElement dataElement;
+
+	static constraints = {
+		period(blank:false,nullable:false)
+		dataElement(blank:false,nullable:false)
+		file(blank:false,nullable:false, validator: { val, obj ->
+
+			final String FILE_TYPE = "text/csv";
+			boolean valid = true;
+			if(val != null)
+				if(!val.contentType.equals(FILE_TYPE))
+					return valid=false;
+			return valid;
+		})
+	}
+}
---
+++ grails-app/views/import/normalizedImport.gsp
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="layout" content="main" />
+		<title><g:message code="import.form.label" default="Data Import" /></title>
+		<!-- for admin forms -->
+        <r:require modules="form,chosen"/>
+	</head>
+	<body>
+		<div class="entity-form-container togglable">
+			<div class="entity-form-header">
+				<h3 class="title">Import Data</h3>
+				<div class="clear"></div>
+			</div>
+				<g:form url="[controller:'normalizedImporterEntity', action:'uploader']" useToken="true" method="post" enctype="multipart/form-data">
+					<g:selectFromList name="dataElement.id" label="Data element" bean="${normalizedImporterEntity}" field="dataElement" optionKey="id" multiple="false"
+					ajaxLink="${createLink(controller:'data', action:'getAjaxData', params:[class:'DataElement'])}"
+					from="${dataElements}" value="${importerEntity?.dataElement?.id}" values="${dataElements.collect{i18n(field:it.names)+' ['+it.code+'] ['+it.class.simpleName+']'}}" />
+					
+					<g:selectFromList name="period.id" label="Period" bean="${normalizedImporterEntity}" field="period" optionKey="id" multiple="false"
+					from="${periods}" value="${importerEntity?.period?.id}" optionValue="startDate" />
+					
+					<g:file name="file" label="File" bean="${normalizedImporterEntity}" field="file"/>
+					<div class="row">
+						<button type="submit"><g:message code="default.button.import.label" default="Import"/></button>&nbsp;&nbsp;
+						<a href="${createLink(uri: targetURI)}"><g:message code="default.link.cancel.label" default="Cancel"/></a>
+					</div>
+				</g:form>
+		</div>
+
+	</body>
+</html>
