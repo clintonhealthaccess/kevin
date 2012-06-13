738a9f2a2f22f30a41e59feb755fb9b56e8012d7
François Terrier <fterrier@gmail.com>
2012-06-13 08:40:30 +0200

    fixed bug where missing name for language was causing a NPE

---
+++ test/unit/org/chai/kevin/planning/PlanningCostUnitSpec.groovy
@@ -0,0 +1,45 @@
+package org.chai.kevin.planning
+
+import org.chai.kevin.LanguageService;
+import org.chai.kevin.UnitTests;
+
+import grails.plugin.spock.UnitSpec;
+
+class PlanningCostUnitSpec extends UnitTests {
+
+	def "get groups and display name"() {
+		when:
+		def languageService = new LanguageService()
+		languageService.metaClass.getCurrentLanguage = {"en"}
+		def planningCost = new PlanningCost(names: j(["en": "Cost - Revenue"]))
+		
+		then:
+		planningCost.getGroups(languageService) == ["Cost"]
+		planningCost.getDisplayName(languageService) == "Revenue"
+	}
+	
+	def "get groups and display on default langauge"() {
+		when:
+		def languageService = new LanguageService()
+		languageService.metaClass.getCurrentLanguage = {"fr"}
+		languageService.metaClass.getFallbackLanguage = {"en"}
+		def planningCost = new PlanningCost(names: j(["en": "Cost - Revenue"]))
+		
+		then:
+		planningCost.getGroups(languageService) == ["Cost"]
+		planningCost.getDisplayName(languageService) == "Revenue"
+	}
+	
+	def "get groups and display on empty name"() {
+		when:
+		def languageService = new LanguageService()
+		languageService.metaClass.getCurrentLanguage = {"fr"}
+		languageService.metaClass.getFallbackLanguage = {"en"}
+		def planningCost = new PlanningCost(names: j([:]))
+		
+		then:
+		planningCost.getGroups(languageService) == []
+		planningCost.getDisplayName(languageService) == ""
+	}
+	
+}
