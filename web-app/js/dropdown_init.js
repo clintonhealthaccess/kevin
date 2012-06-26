86168ea27cab3f13e11eaa52a0166bc679b4c8ad
François Terrier <fterrier@gmail.com>
2012-06-26 11:58:27 +0200

    fixed bug where filter would not close if clicking on it

--- web-app/js/dropdown_init.js
+++ web-app/js/dropdown_init.js
@@ -2,28 +2,28 @@
  * drop-down menus
  * TODO transform in jQuery Plugin style
  **/
-$(document).delegate('.js_dropdown .selected', 'click', function(e) {
-	$(".js_dropdown-list").hide();
-	$(this).parents(".js_dropdown").find(".js_dropdown-list").toggle();
-	e.stopPropagation();
-	return false;
-});
-$(".js_dropdown .js_dropdown-link").bind('click', function(e) {
-	reset(e.target);
-	$(this).parents('.js_dropdown').find('.js_dropdown-list').toggle();
-	$(this).parents('.js_dropdown').find('a').toggleClass('dropdown-selected');
-	if ($(this).attr('href') == '#') return false;
+
+$(document).delegate(".js_dropdown a.js_dropdown-link").bind('click', function(e) {
+	if ($(e.target).hasClass('dropdown-selected')) {
+		reset(null);
+	}
+	else {
+		reset(e.target);
+		$(e.target).addClass('dropdown-selected');
+		$(e.target).parents('.js_dropdown').find('.js_dropdown-list').show();
+		if ($(e.target).attr('href') == '#') return false;
+	}
 });
 $(document).bind('click', function(e) {
 	if (!$(e.target).hasClass('js_dropdown-ignore')) {
-		reset(e.target)
+		//reset(e.target)
 	}	
 });
 
 function reset(clicked) {
-	$(".js_dropdown a.js_dropdown-link").each(function(){
+	$(".js_dropdown .js_dropdown-link").each(function(){
 		if (clicked != this) {
-			$(this).parents(".js_dropdown").find("div.js_dropdown-list").hide();
+			$(this).parents(".js_dropdown").find(".js_dropdown-list").hide();
 			$(this).parents(".js_dropdown").find(".dropdown-selected").removeClass('dropdown-selected');
 		}
 	});
