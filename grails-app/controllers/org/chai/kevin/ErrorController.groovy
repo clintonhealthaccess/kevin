d95b038eb83496e151805b1be9bd93a61ea224db
François Terrier <fterrier@gmail.com>
2012-09-05 12:48:15 +0200

    sending an email on the 500 errors

---
+++ grails-app/controllers/org/chai/kevin/ErrorController.groovy
@@ -0,0 +1,22 @@
+import grails.util.GrailsUtil;
+
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+class ErrorController {
+
+	def internalError = {
+		// admin email is specified in the Config.groovy file
+		// An email will be sent to the admin person whenever an internalError occurred.
+		def adminEmail = ConfigurationHolder.config.site.admin.email
+		def exception = request.exception
+		
+		sendMail {
+			multipart true
+			to adminEmail
+			subject "Unhandled exception in the production environment"
+			html g.renderException(exception: exception)
+		}
+		
+		render(view: '/error')
+	}
+}
