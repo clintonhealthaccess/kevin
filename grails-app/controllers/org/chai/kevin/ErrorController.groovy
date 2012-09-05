d5db272ca1f928acdc36e5af305bf5d83f96a3f0
François Terrier <fterrier@gmail.com>
2012-09-05 15:11:23 +0200

    added package name

--- grails-app/controllers/org/chai/kevin/ErrorController.groovy
+++ grails-app/controllers/org/chai/kevin/ErrorController.groovy
@@ -1,5 +1,6 @@
-import grails.util.GrailsUtil;
+package org.chai.kevin
 
+import grails.util.GrailsUtil;
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class ErrorController {
@@ -9,11 +10,13 @@ class ErrorController {
 			// admin email is specified in the Config.groovy file
 			// An email will be sent to the admin person whenever an internalError occurred.
 			def adminEmail = ConfigurationHolder.config.site.admin.email
+			def fromEmail = ConfigurationHolder.config.site.from.email;
 			def exception = request.exception
 			
 			sendMail {
 				multipart true
 				to adminEmail
+				from fromEmail
 				subject "Unhandled exception in the production environment"
 				html g.renderException(exception: exception)
 			}
