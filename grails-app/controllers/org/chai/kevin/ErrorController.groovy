371a73f2591f959f15bcfbe7416651b55eca3b1c
François Terrier <fterrier@gmail.com>
2012-09-05 14:27:59 +0200

    added exception check

--- grails-app/controllers/org/chai/kevin/ErrorController.groovy
+++ grails-app/controllers/org/chai/kevin/ErrorController.groovy
@@ -5,18 +5,20 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 class ErrorController {
 
 	def internalError = {
-		// admin email is specified in the Config.groovy file
-		// An email will be sent to the admin person whenever an internalError occurred.
-		def adminEmail = ConfigurationHolder.config.site.admin.email
-		def exception = request.exception
-		
-		sendMail {
-			multipart true
-			to adminEmail
-			subject "Unhandled exception in the production environment"
-			html g.renderException(exception: exception)
+		if (request.exception) {
+			// admin email is specified in the Config.groovy file
+			// An email will be sent to the admin person whenever an internalError occurred.
+			def adminEmail = ConfigurationHolder.config.site.admin.email
+			def exception = request.exception
+			
+			sendMail {
+				multipart true
+				to adminEmail
+				subject "Unhandled exception in the production environment"
+				html g.renderException(exception: exception)
+			}
+			
+			render (view: 'error')
 		}
-		
-		render (view: 'error')
 	}
 }
